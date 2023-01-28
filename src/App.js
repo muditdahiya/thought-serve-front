022556cb88e4be25453fd1dee17fa9aace4218c8
muditdahiya <mdtheroxtar@gmail.com>
2023-01-28 01:36:26 -0500

    fixed routing

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { BrowserRouter } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="thought-serve">
         <Header />
         <Main />
         <Footer />
