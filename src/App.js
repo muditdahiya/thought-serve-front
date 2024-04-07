502ba8c3b14032768100e0dd1b362ac54318cd92
muditdahiya <muditd99@gmail.com>
2024-04-06 23:32:24 -0400

    build with https environment

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { BrowserRouter } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="thought-serve">
+      <BrowserRouter basename="/thought-serve">
         <Header />
         <Main />
         <Footer />
